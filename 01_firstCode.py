2953317bed56425ae0f1d1fa24b6e76781d5100d
PanshopMaster <juanfranciscogalaz@gmail.com>
2025-02-05 12:28:46 -0300

    agrego class trabajador

--- 01_firstCode.py
+++ 01_firstCode.py
@@ -1,7 +1,21 @@
 # This program takes user input and prints it
 
 # Taking input from the user
-user_input = input("Enter something: ")
+#user_input = input("Enter something: ")
 
 # Printing the user input
-print("You entered:", user_input)
+#print("You entered:", user_input)
+
+
+class Trabajador:
+    def __init__(self, name, grad, age):
+        self.name = name
+        self.grad = grad
+        self.age = age
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.name}, Puesto: {self.grad}, Edad: {self.age}")
+
+# Ejemplo de uso
+trabajador1 = Trabajador("Juan", "Ingeniero", "22")
+trabajador1.mostrar_informacion()
