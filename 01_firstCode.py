ae0c5aee0989921de61619862af5834c801ddfc9
PanshopMaster <juanfranciscogalaz@gmail.com>
2025-02-05 12:13:53 -0300

    segundo cambio

---
+++ 01_firstCode.py
@@ -0,0 +1,7 @@
+# This program takes user input and prints it
+
+# Taking input from the user
+user_input = input("Enter something: ")
+
+# Printing the user input
+print("You entered:", user_input)
