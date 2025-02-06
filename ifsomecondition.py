b8759747c61993d3e43ea98d9e03f289cfbe34dc
PanshopMaster <juanfranciscogalaz@gmail.com>
2025-02-06 12:10:29 -0300

    add new example

---
+++ ifsomecondition.py
@@ -0,0 +1,10 @@
+#if = do some code only if some condition is true
+#else = do some code only if the condition is false
+import os
+
+age = int(input("Enter your age: "))
+if age < 18:
+    print("You are a minor")
+else:
+    print("You are an adult")
+   
--- triangulorectagulomath.py
+++ triangulorectagulomath.py
@@ -1,4 +1,6 @@
 import math
+import os.system
+
 a= float(input("Enter the value of a: "))
 b = float(input("Enter the value of b: "))
 
